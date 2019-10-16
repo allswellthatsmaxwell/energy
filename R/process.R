a0a999d8a4a5ed02e4bc02ae62c9a039683a611e
Maxwell Peterson <allswellthatsmaxwell9@gmail.com>
2019-10-16 00:42:21 -0500

    reading data; initial exploration

--- R/process.R
+++ R/process.R
@@ -1,5 +1,6 @@
 library(glue)
 library(ggplot2)
+library(magrittr)
 
 DATA_DIR <- '../data'
 
@@ -13,8 +14,31 @@ files <- list(
     weather = list(train = data_file('weather_train.csv'),
                    test = data_file('weather_test.csv')),
     buildings = data_file('building_metadata.csv'))
-)
 
 buildings <- readr::read_csv(files$buildings)
 meter <- readr::read_csv(files$meter$train)
-weather <- files$weather(files$weather$train)
+weather <- readr::read_csv(files$weather$train) %>%
+  dplyr::mutate(site_id = factor(site_id))
+
+weather %<>% 
+  dplyr::mutate(day = as.integer(format(timestamp, "%d")),
+                week = as.integer(format(timestamp, "%U")))
+
+buildings_per_site <- buildings %>%
+  dplyr::group_by(site_id) %>%
+  dplyr::summarize(dplyr::n())
+buildings_per_site
+
+## No, this isn't weekly! Rows in weather are by the minute, not the day.
+#weather %<>% 
+#  dplyr::arrange(site_id, timestamp) %>%
+#  dplyr::group_by(site_id) %>%
+#  dplyr::mutate(weekly_avg_air_temperature = zoo::rollapply(
+#    air_temperature, 7, mean, align='left', fill=NA))
+  
+site_temps_plot <- weather %>%
+  ggplot(aes(x = timestamp, y = weekly_avg_air_temperature)) +
+  geom_line() +
+  theme_bw() +
+  facet_wrap(~site_id)
+site_temps_plot
--- R/setup.R
+++ R/setup.R
@@ -1,2 +1,7 @@
-install.packages("devtools")
+packages <- c(
+  "glue", "readr", "dplyr", "magrittr", "ggplot2",
+  "purrr", "tidyr", "zoo",
+  "devtools"
+)
+install.packages(packages)
 devtools::install_github("rstudio/keras")
